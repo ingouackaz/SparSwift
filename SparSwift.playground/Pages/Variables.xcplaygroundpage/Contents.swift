147e91a5490cf205c6f7277e7036ead4c767849f
Roger Ingouacka <roger.ingouacka@gmail.com>
2016-11-14 14:49:09 +0100

    Add correction & a few things

--- SparSwift.playground/Pages/Variables.xcplaygroundpage/Contents.swift
+++ SparSwift.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -5,12 +5,12 @@
  */
 //:
 /*:
-//: En swift, on utilise des variables pour stocker  et manipuler des informations comme : un prénom , un nom, l'age d'une personne ou encore son adesse mail.
+//: En swift, on utilise des variables pour stocker  et manipuler des informations comme un prénom , un nom, l'age d'une personne ou encore son adesse mail.
 //: Toutes ces variables possèdent un type relatif à leurs contenu
 //:
 //: Les principaux types de variables sont :
 //:
-//: * Les `String` -> Pour stocker desvaleurs de type texte
+//: * Les `String` -> Pour stocker des valeurs de type texte
 //:
 //: * Les `Int` -> Pour stocker des valeurs de type nombre entier
 //:
@@ -33,114 +33,120 @@ var myOld : Int = 24
 
 //: En français les lignes ci-dessus se traduisent  par
 //:
-//: * Ligne 30 : _Je déclare une variable que j'appelle `myName` de type `String` . Celle ci contiend le texte `Roger`_
+//: * Ligne 30 : _Je *déclare* une variable que j'appelle `myName` de type `String` et je lui assigne le texte `Roger`_
 //:
-//: * Ligne 31 : _Je déclare une variable que j'appelle `myLastName` de type `String` . Celle ci contiend le texte `Federer`_
+//: * Ligne 31 : _Je déclare une variable que j'appelle `myLastName` de type `String` et je lui assigne le texte `Federer`_
 //:
-//: * Ligne 32 : _Je déclare une variable que j'appelle `myOld` de type `Int` . Celle ci contiend le nombre entier `24`_
+//: * Ligne 32 : _Je déclare une variable que j'appelle `myOld` de type `Int` et je lui assigne le nombre entier `24`_
 //:
 //: > **Conseil** :
 //: N'hésitez pas à traduire les lignes de codes que vous verrez durant tout le programme , cela est très bénéfique pour la compréhension du language
-//: Le contenu d'une variable définit le type de celle-ci.
 //:
+//: Voyons si ces quelques notions sont acquises :)
 /*:
  ## LA PRATIQUE
  */
+//:
+//: --------------------------------
+//: **Exercice 0**
+//:
+//: Créez une variable qu'on appellera `strExample` de type `String` qui contiendra la valeur _"Premier exemple"_
+//:
+var strExample : String = "Premier exemple"
+//: > **À savoir** :
+//: Pour vérifier si l'exercice est correct il faut faire appel aux correcteurs de cette maniere : `SparSwift_correct_my_exercice_number(strExample)`
+SparSwift_correct_my_exercice_0(string: strExample)
+//: > **À savoir** :
+//: Vérifier ensuite la correction de votre exercice dans la console ci dessous :) 
+//:  Good luck !
+//: --------------------------------
 //: **Exercice 1**
 //:
 //: Créez une variable qu'on appellera `str` de type `String` qui contiendra la valeur _"Salut tout le monde"_
 var str : String = "Salut les amis"
+SparSwift_correct_my_exercice_1(string: str)
+
 // SparSwifter_exo_2(word:str)
-//:
+//: --------------------------------
 //: **Exercice 2**
 //:
 //: Créez une variable qu'on appellera `isCool` de type `Bool` qui contiendra la valeur _true_
-var str1 : String = "Salut les amis"
+// var
 // SparSwifter_exo_2(word:str)
-//:
+
+//: --------------------------------
 //: **Exercice 3**
 //:
 //: Créez une variable qu'on appellera `num` de type `Int` qui contiendra la valeur _23_
 var num : Int = 0
 // SparSwifter_exo_2(word:str)
-//:
+//: --------------------------------
 //: **Exercice 4**
 //:
 //: Créez une variable qu'on appellera `taille` de type `Double` qui contiendra la valeur _1.80_
 var tailleEnMetre : String = "Salut les amis"
 // SparSwifter_exo_2(word:str)
-//:
+///: --------------------------------
 //: **Exercice 5**
 //:
 //: Créez 2 variables : une qu'on appellera  name de type String et une autre qu'on appellera lastname de type String. Elles contiendront respectivements les valeus _"Roger"_ et _"Federer"_
 var str4 : String = "Salut les amis"
 // SparSwifter_exo_2(word:str)
+//: --------------------------------
 //: **Exercice 6**
 //:
 //: Créez une variable qu'on appelera isCool de type Bool. isCool contiendra la valeur true
 //:
 // SparSwifter_exo_2(word:str)
-//:
-//: **Exercice 6**
+//: --------------------------------
+//: **Exercice 7**
 //:
 //: Vrai ou Faux : On peut la valaur 1920 dans une variable de type String
 //:
 // SparSwifter_exo_2(word:str)
-//:
-//: **Exercice 7**
+//: --------------------------------
+//: **Exercice 8**
 //:
 //: Vrai ou Faux : On peut stocker la valeur "Terre" dans une variable de type String
 //:
 // SparSwifter_exo_2(word:str)
-//:
-//: **Exercice 8**
+//: --------------------------------
+//: **Exercice 9**
 //:
 //: Vrai ou Faux : Pour stocker un nombre à virgule je peux utiliser un Int
 //:
 // SparSwifter_exo_2(word:str)
-//:
-//: **Exercice 9**
+//: --------------------------------
+//: **Exercice 10**
 //:
 //: Vrai ou Faux : Pour stocker un nombre à virgule je peux utiliser un Inr
 //:
 // SparSwifter_exo_2(word:str)
-//:
-//: **Exercice 10**
+//: --------------------------------
+//: **Exercice 11**
 //:
 //: Cette ligne est correct  :  _var name : string = "Michael"_
 //:
 // SparSwifter_exo_2(word:str)
-//:
-//: **Exercice 11**
+//: --------------------------------
+//: **Exercice 12**
 //:
 //: Cette ligne est correct  :  _var pseudo : String = "007"_
 //:
 // SparSwifter_exo_2(word:str)
-//:
-//: **Exercice 12**
+//: --------------------------------
+//: **Exercice 13**
 //:
 //: Cette ligne est correct  :  _let petitPainPrice : Double = 1.40
 //:
 // SparSwifter_exo_2(word:str)
 //:
-//: **Exercice 13**
+//: --------------------------------
+//: **Exercice 14**
 //:
 //: Cette ligne est correct  :  _let  : string = "Michael"_
 //:
 // SparSwifter_exo_2(word:str)
 //:
-/*:
- > A great text.
- */
-
-
-
-
-
-
-
-
-
-//:
-//: [Previous](@previous) | [Next](@next)
+//: [Précédent](@previous) | [Suivant](@next)
 
---
+++ SparSwift.playground/Pages/Variables.xcplaygroundpage/Sources/Library.swift
@@ -0,0 +1,172 @@
+import Foundation
+
+
+var formatExOutput : String = ""
+
+public func SparSwift_correct_my_exercice_0(string:String){
+
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+
+    }
+}
+
+public func SparSwift_correct_my_exercice_1(string:String){
+    
+    if (string == "Salut les amis"){
+        print("Exercice [1] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [1] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_2(bool:Bool){
+    
+    if (bool == true){
+        print("Exercice [2] - ACCOMPLI - Yeessss ! Le Swift c'est cool :)")
+    }
+    else {
+        print("Exercice [2] - ÉCHOUÉ - Comment ça ... c'est vraiment nul le Swift ? :(")
+    }
+}
+
+
+public func SparSwift_correct_my_exercice_3(num:Int){
+  
+    // consigne  : Créez une variable qu'on appellera num de type Int qui contiendra la valeur 23
+
+    if (num == 23){
+        print("Exercice [3] - ACCOMPLI - :)")
+    }
+    else {
+        print("Exercice [3] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_4(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_5(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_6(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_7(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_8(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_9(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_10(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+
+public func SparSwift_correct_my_exercice_11(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_12(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_13(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
+
+public func SparSwift_correct_my_exercice_14(string:String){
+    
+    if (string == "Premier exemple"){
+        print("Exercice [0] - ACCOMPLI :)")
+    }
+    else {
+        print("Exercice [0] - ÉCHOUÉ :(")
+        
+    }
+}
