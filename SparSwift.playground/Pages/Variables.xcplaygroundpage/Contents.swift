1fb70462b5a84b31b794a8f4a2d8451f69d98d63
Roger Ingouacka <roger.ingouacka@gmail.com>
2016-11-14 01:12:31 +0100

    coo playground

---
+++ SparSwift.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,146 @@
+
+
+/*:
+ # LA BASE
+ */
+//:
+/*:
+//: En swift, on utilise des variables pour stocker  et manipuler des informations comme : un prénom , un nom, l'age d'une personne ou encore son adesse mail.
+//: Toutes ces variables possèdent un type relatif à leurs contenu
+//:
+//: Les principaux types de variables sont :
+//:
+//: * Les `String` -> Pour stocker desvaleurs de type texte
+//:
+//: * Les `Int` -> Pour stocker des valeurs de type nombre entier
+//:
+//: * Les `Double` -> Pour stocker des valeurs de type nombre décimal
+//:
+//: * Les `Float` -> Pour stocker des valeurs de type nombre décimal ( avec plus de précision qu'un float )
+//:
+//: * Les `Bool` -> Pour stocker des valeurs de type vrai ou faux
+//: > **À savoir** :
+//: Il existe beaucoup d'autres type de variable en Swift, n'ayez crainte nous verrons tout cela dans d'autres chapitres ;)
+ */
+/*:
+ ## LA SYNTAXE
+ */
+//: Pour déclarer une variable, on utilise le mot clé `var` suivi du `nomDeLaVariable` de son `type` puis  enfin de la `valeur` qu'on souhaite lui assigner.
+//:
+ var myName : String = "Roger"
+var myLastName : String = "Federer"
+var myOld : Int = 24
+
+//: En français les lignes ci-dessus se traduisent  par
+//:
+//: * Ligne 30 : _Je déclare une variable que j'appelle `myName` de type `String` . Celle ci contiend le texte `Roger`_
+//:
+//: * Ligne 31 : _Je déclare une variable que j'appelle `myLastName` de type `String` . Celle ci contiend le texte `Federer`_
+//:
+//: * Ligne 32 : _Je déclare une variable que j'appelle `myOld` de type `Int` . Celle ci contiend le nombre entier `24`_
+//:
+//: > **Conseil** :
+//: N'hésitez pas à traduire les lignes de codes que vous verrez durant tout le programme , cela est très bénéfique pour la compréhension du language
+//: Le contenu d'une variable définit le type de celle-ci.
+//:
+/*:
+ ## LA PRATIQUE
+ */
+//: **Exercice 1**
+//:
+//: Créez une variable qu'on appellera `str` de type `String` qui contiendra la valeur _"Salut tout le monde"_
+var str : String = "Salut les amis"
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 2**
+//:
+//: Créez une variable qu'on appellera `isCool` de type `Bool` qui contiendra la valeur _true_
+var str1 : String = "Salut les amis"
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 3**
+//:
+//: Créez une variable qu'on appellera `num` de type `Int` qui contiendra la valeur _23_
+var num : Int = 0
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 4**
+//:
+//: Créez une variable qu'on appellera `taille` de type `Double` qui contiendra la valeur _1.80_
+var tailleEnMetre : String = "Salut les amis"
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 5**
+//:
+//: Créez 2 variables : une qu'on appellera  name de type String et une autre qu'on appellera lastname de type String. Elles contiendront respectivements les valeus _"Roger"_ et _"Federer"_
+var str4 : String = "Salut les amis"
+// SparSwifter_exo_2(word:str)
+//: **Exercice 6**
+//:
+//: Créez une variable qu'on appelera isCool de type Bool. isCool contiendra la valeur true
+//:
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 6**
+//:
+//: Vrai ou Faux : On peut la valaur 1920 dans une variable de type String
+//:
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 7**
+//:
+//: Vrai ou Faux : On peut stocker la valeur "Terre" dans une variable de type String
+//:
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 8**
+//:
+//: Vrai ou Faux : Pour stocker un nombre à virgule je peux utiliser un Int
+//:
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 9**
+//:
+//: Vrai ou Faux : Pour stocker un nombre à virgule je peux utiliser un Inr
+//:
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 10**
+//:
+//: Cette ligne est correct  :  _var name : string = "Michael"_
+//:
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 11**
+//:
+//: Cette ligne est correct  :  _var pseudo : String = "007"_
+//:
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 12**
+//:
+//: Cette ligne est correct  :  _let petitPainPrice : Double = 1.40
+//:
+// SparSwifter_exo_2(word:str)
+//:
+//: **Exercice 13**
+//:
+//: Cette ligne est correct  :  _let  : string = "Michael"_
+//:
+// SparSwifter_exo_2(word:str)
+//:
+/*:
+ > A great text.
+ */
+
+
+
+
+
+
+
+
+
+//:
+//: [Previous](@previous) | [Next](@next)
+
